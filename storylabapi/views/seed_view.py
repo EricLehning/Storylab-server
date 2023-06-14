6f66ae83d83994e5c8b0880f4c260490e3bcc288
Eric Lehning <ellehning@gmail.com>
2023-06-14 14:23:26 -0500

    tidy up

---
+++ storylabapi/views/seed_view.py
@@ -0,0 +1,36 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from storylabapi.models import Seed
+
+
+class SeedView(ViewSet):
+    """Story Lab Seed view"""
+    """Handle GET requests for single seed 
+    Returns:
+        Response -- JSON serialized seed
+    """
+    def retrieve(self, request, pk):
+        seed = Seed.objects.get(pk=pk)
+        serializer = SeedSerializer(seed)
+        return Response(serializer.data)
+        
+
+
+    def list(self, request):
+        """Handle GET requests to get all seeds
+        Returns:
+            Response -- JSON serialized list of seeds
+        """
+        seeds = Seed.objects.all()
+        serializer = SeedSerializer(seeds, many=True)
+        return Response(serializer.data)
+    
+    class SeedSerializer(serializers.ModelSerializer):
+        """JSON serializer for game types
+        """
+        class Meta:
+            model = Seed
+            fields = ('id', 'label')
